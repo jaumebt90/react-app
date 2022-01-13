1b39d02e5a1e2815ba38c8285828e2f3c564e387
Jaume <ballestertomasj@gmail.com>
2022-01-13 14:26:26 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Button from "./Button";
 
+const arr = ["chanchito feliz", "chanchito triste", "chanchito emocionado"];
+
 const App = () => {
-  const miVariable = true;
+  const miVariable = false;
   if (miVariable) {
-    return <p>Mi variable dio true</p>;
+    return <p>Mi variable dio true!</p>;
   }
   return (
     <div>
       <h1 onClick={(e) => console.log("click", e)}>Hola mundo</h1>
+      {arr.map((el) => (
+        <p key={el}>{el}</p>
+      ))}
       <Button onClick={() => console.log("Clickeado")}>Enviar</Button>
     </div>
   );
