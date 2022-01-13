00609ddbef314d6bc484fe618a001cba7bfdc549
Jaume <ballestertomasj@gmail.com>
2022-01-13 14:22:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import Button from "./Button";
 
 const App = () => {
+  const miVariable = true;
+  if (miVariable) {
+    return <p>Mi variable dio true</p>;
+  }
   return (
     <div>
       <h1 onClick={(e) => console.log("click", e)}>Hola mundo</h1>
