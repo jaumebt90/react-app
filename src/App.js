7a095f0c46f2edef62b8941443ead7a2da118f55
Jaume <ballestertomasj@gmail.com>
2022-01-13 14:15:32 +0100

    estilos

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import logo from "./logo.svg";
-//import "./App.css";
-import "./main.css";
+import Button from "./Button";
 
-const estilo2 = {
-  boxShadow: "0 5px 3px rgba(0,0,0,0.5)",
-};
-
-const estilo = ({ bg = "#333" }) => ({
-  backgroundColor: bg,
-  color: "#fff",
-  padding: "10px 15px",
-  margin: "10px 15px",
-});
-const Li = ({ children }) => {
-  return <li className="clase-li">{children}</li>;
-};
-
-function App() {
-  const valor = "triste";
+const App = () => {
   return (
-    <ul className="clase-css">
-      <Li estado="feliz">valor de li</Li>
-    </ul>
+    <div>
+      <h1>Hola mundo</h1>
+      <Button onClick={() => console.log("Clickeado")}>Enviar</Button>
+    </div>
   );
-}
+};
 
 export default App;
