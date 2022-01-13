2143c5cb16e747b5e15c6a143e0f1fc94d58abb6
Jaume <ballestertomasj@gmail.com>
2022-01-13 13:56:37 +0100

    estilos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
-import "./App.css";
+//import "./App.css";
+import "./main.css";
 
 const estilo2 = {
   boxShadow: "0 5px 3px rgba(0,0,0,0.5)",
@@ -12,17 +13,13 @@ const estilo = ({ bg = "#333" }) => ({
   margin: "10px 15px",
 });
 const Li = ({ children }) => {
-  return (
-    <li style={{ ...estilo2, ...estilo({ bg: "#333" }) }} className="clase-li">
-      {children}
-    </li>
-  );
+  return <li className="clase-li">{children}</li>;
 };
 
 function App() {
   const valor = "triste";
   return (
-    <ul style={estilo({ bg: "#750" })} className="clase-css">
+    <ul className="clase-css">
       <Li estado="feliz">valor de li</Li>
     </ul>
   );
